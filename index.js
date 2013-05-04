b9abcac3ac0f81f2603af3a8435fafeebaeaa6bf
ka <ka.kaosf@gmail.com>
2013-05-05 00:30:19 +0900

    implement the simple proxy server feature

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+httpProxy = require('http-proxy');
+
 module.exports = {
   startProxyServer: function() {
-    console.log('Proxy server is started...');
+    httpProxy.createServer(function(req, res, proxy) {
+      proxy.proxyRequest(req, res, {
+        host: 'localhost',
+        port: 9000
+      });
+    }).listen(10000);
+    console.log('Starting Server at http://localhost:10000');
   }
 };
